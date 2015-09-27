8fbdd5a5760a1466686c8e3319050baf792f1914
andybao <wenyu.bao@gmail.com>
2015-09-27 22:21:42 +0800

    add speedbar

---
+++ elpa/sr-speedbar-20150804.951/sr-speedbar-autoloads.el
@@ -0,0 +1,16 @@
+;;; sr-speedbar-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("sr-speedbar.el") (22023 62314 446456
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sr-speedbar-autoloads.el ends here
