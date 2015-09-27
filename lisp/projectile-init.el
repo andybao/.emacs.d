882fdcc3656605817ca630db06a503afb9b3f0b2
andybao <wenyu.bao@gmail.com>
2015-09-27 21:14:14 +0800

    add helm projectile

---
+++ lisp/projectile-init.el
@@ -0,0 +1,8 @@
+;;----------------------------------------------------------------------------
+;; Projectile is a project interaction library for Emacs
+;;----------------------------------------------------------------------------
+
+(projectile-global-mode)
+(helm-projectile-on)
+
+(provide 'projectile-init)
