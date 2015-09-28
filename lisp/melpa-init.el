45defaae206d739ea950c796d164897136d6a5b6
andybao <wenyu.bao@gamil.com>
2015-09-28 08:08:29 +0800

    change from stable to common

--- lisp/melpa-init.el
+++ lisp/melpa-init.el
@@ -3,7 +3,8 @@
 ;;---------------------------------------------------------------------------
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
