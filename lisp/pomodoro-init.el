deee9d9ac049e19c946a95b27a8a7dda3bb4408d
andybao <wenyu.bao@gamil.com>
2015-09-29 12:06:45 +0800

    update

--- lisp/pomodoro-init.el
+++ lisp/pomodoro-init.el
@@ -10,17 +10,21 @@
 (require 'pomodoro)
 (pomodoro-add-to-mode-line)
 
+(global-set-key (kbd "C-c t s") 'pomodoro-start)
+(global-set-key (kbd "C-c t p") 'pomodoro-pause)
+(global-set-key (kbd "C-c t e") 'pomodoro-stop)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(pomodoro-break-start-sound "/Users/andybao/.emacs.d/support/pomodoro.mp3")
+ '(pomodoro-break-start-sound "~/.emacs.d/support/pomodoro.mp3")
  '(pomodoro-break-time 5)
  '(pomodoro-play-sounds t)
  '(pomodoro-show-number t)
  '(pomodoro-sound-player "afplay")
- '(pomodoro-work-start-sound "/Users/andybao/.emacs.d/support/pomodoro.mp3")
+ '(pomodoro-work-start-sound "~/.emacs.d/support/pomodoro.mp3")
  '(pomodoro-work-time 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
